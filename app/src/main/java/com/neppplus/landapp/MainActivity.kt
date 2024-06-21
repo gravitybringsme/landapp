ddd305b050f018375e79fc675ebc94266092b8ed
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-21 11:30:30 +0900

    임시로 방 주소 /  설명 반영

--- app/src/main/java/com/neppplus/landapp/MainActivity.kt
+++ app/src/main/java/com/neppplus/landapp/MainActivity.kt
@@ -25,16 +25,16 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        mRoomList.add( Room(7500, "서울시 동대문구", 8, "서울시 동대문구의 8층 7500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구", 0, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구2", 1, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구3", 2, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구4", 3, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구5", 4, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구6", 5, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구7", 6, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구8", 7, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
-        mRoomList.add( Room(24500, "서울시 서대문구9", 8, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(7500, "서울시 동대문구", 8, "동대문구의 8층 7500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구", 0, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구2", 1, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구3", 2, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구4", 3, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구5", 4, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구6", 5, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구7", 6, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구8", 7, "서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구9", 8, "서대문구의 반지하 2억 4500만원 방입니다."))
 
         mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
         findViewById<ListView>(R.id.roomListView).adapter = mRoomAdapter
--- app/src/main/java/com/neppplus/landapp/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/landapp/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.appcompat.widget.ActionBarContextView
 import com.neppplus.landapp.R
 import com.neppplus.landapp.datas.Room
@@ -22,6 +23,15 @@ class RoomAdapter(val mContext: Context,
         }
 
         val row = tempRow!!
+
+        val roomData = mList[position]
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+        descriptionTxt.text = roomData.description
+        addressAndFloorTxt.text = roomData.address
 
         return row
     }
