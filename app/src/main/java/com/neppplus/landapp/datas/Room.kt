eefd69d0e7860a58caa5afe09e503320e7def54b
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-21 11:45:28 +0900

    방의 가격 가공 기능 제작/사용

--- app/src/main/java/com/neppplus/landapp/datas/Room.kt
+++ app/src/main/java/com/neppplus/landapp/datas/Room.kt
@@ -1,11 +1,30 @@
 package com.neppplus.landapp.datas
 
+import java.text.NumberFormat
+import java.util.Locale
+
 class Room (
     val price : Int,
     val address:String,
     val floor : Int,
     val description:String)
 {
+
+    fun getFormattedPrice():String {
+        if (this.price >= 10000)
+        {
+            val uk = this.price / 10000 // 정수와 정수의 나눗셈은 소수점을 버리고 몫만
+            val rest = this.price % 10000
+            return "${uk}억 ${NumberFormat.getNumberInstance(Locale.KOREA).format(rest)}"
+        }
+        else {
+
+            return "${NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)}"
+        }
+    }
+
+
+
     fun getFormattedFloor():String {
         if (this.floor >= 1)
         {
