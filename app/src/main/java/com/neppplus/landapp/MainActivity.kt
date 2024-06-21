18e8329043f7aeef2bd69676a97341261b3e1956
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-21 11:17:04 +0900

    방 목록 추가 / 어댑터 연결

--- app/src/main/java/com/neppplus/landapp/MainActivity.kt
+++ app/src/main/java/com/neppplus/landapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.neppplus.landapp
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.neppplus.landapp.adapters.RoomAdapter
+import com.neppplus.landapp.datas.Room
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList= ArrayList<Room>()
+
+    lateinit var mRoomAdapter : RoomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        mRoomList.add( Room(7500, "서울시 동대문구", 8, "서울시 동대문구의 8층 7500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구", 0, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구2", 1, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구3", 2, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구4", 3, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구5", 4, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구6", 5, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구7", 6, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구8", 7, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+        mRoomList.add( Room(24500, "서울시 서대문구9", 8, "서울시 서대문구의 반지하 2억 4500만원 방입니다."))
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+        findViewById<ListView>(R.id.roomListView).adapter = mRoomAdapter
     }
 }
