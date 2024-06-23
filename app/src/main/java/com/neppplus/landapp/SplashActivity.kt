514d2dbfe2ea2f309e13fca6bdd5c274c59a3d39
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-23 18:20:23 +0900

    핸들러를 이용해서 2.5초 뒤에 메인으로 이동

---
+++ app/src/main/java/com/neppplus/landapp/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.neppplus.landapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //각종데이터 확인
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+                              //2.5초 뒤에 실행할 내용
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+        }, 2500)
+    }
+}
